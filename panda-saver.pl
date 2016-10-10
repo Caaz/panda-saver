499c65c6a53d45d44fc7d7bcb01fea01cf98e5b4
Daniel Caaz <daniel@caaz.me>
2016-10-10 14:02:52 -0700

    Sanitize folder structure and filenames.

--- panda-saver.pl
+++ panda-saver.pl
@@ -12,14 +12,14 @@ my @config_keys = ('directory','email','password');
 sub getInput($) { print "\e[33m$_[0]\e[39m"; my $i; chomp($i = <STDIN>); return $i; }
 sub stringifyArray($) { return '['.(join ", ", @{$_[0]}).']'; }
 sub touchDir($) { if(!-e $_[0]) { print "\e[90mCreating directory: $_[0]\e[39m\n"; mkdir($_[0]) or die $!; } }
+sub sanitize { my $text = shift; $text =~ s/[\/&]/_/gs; return $text; }
 sub fall {
   if($config{downloading}) { print "\e[91m\nRemoving partial file: $config{downloading}\e[39m\n"; unlink($config{downloading}); }
   exit 0;
 }
 sub writeTags {
   my $track = shift;
-  my $mp3 = MP3::Tag->new(shift); # create object
-  # $mp3->get_tags(); # read tags
+  my $mp3 = MP3::Tag->new(shift);
   $mp3->new_tag("ID3v1");
   if (exists $mp3->{ID3v1}) {
     $mp3->{ID3v1}->title($track->{songName});
@@ -27,23 +27,25 @@ sub writeTags {
     $mp3->{ID3v1}->album($track->{albumName});
     $mp3->{ID3v1}->write_tag();
   }
-  $mp3->close(); # destroy object
+  $mp3->close();
 }
 sub save($) {
   my $track = $_[0];
   if($track->{audioUrl} && $track->{songName} && $track->{artistName} && $track->{albumName}) {
-    touchDir($config{directory});
-    touchDir(join "/", ($config{directory},$track->{artistName}));
-    touchDir(join "/", ($config{directory},$track->{artistName},$track->{albumName}));
+    my @folders = ($track->{artistName},$track->{albumName});
+    for(@folders) { $_ = sanitize($_); }
+    touchDir(join "/", ($config{directory},$folders[0]));
+    touchDir(join "/", ($config{directory},$folders[0],$folders[1]));
     my $extension = $track->{audioUrl};
     $extension =~ s/^.*(\.[^.]{3})\?.*$/$1/gs;
     my $filename = $track->{songName}.$extension;
-    $filename =~ s/\//_/g;
-    $config{downloading} = (join "/", ($config{directory},$track->{artistName},$track->{albumName},$filename));
+    $filename = sanitize($filename);
+    $config{downloading} = (join "/", ($config{directory},$folders[0],$folders[1],$filename));
     if(!-e $config{downloading}) {
       print "\e[94mSaving $config{downloading}\e[39m\n";
       getstore($track->{audioUrl},$config{downloading});
       writeTags($track,$config{downloading});
+      # system('notify-send','Panda','Downloaded '.$track->{songName}.' by '.$track->{artistName});
     }
   }
 }
@@ -52,6 +54,7 @@ foreach('ABRT','QUIT','KILL','INT','ABRT','HUP') { $SIG{$_} = \&fall; }
 for(my $i = 0; $i < @ARGV; $i++){ $config{$config_keys[$i]} = $ARGV[$i]; }
 for my $key (@config_keys) { $config{$key} = getInput("$key: ") if(!$config{$key}); }
 $config{directory} =~ s/\/$//gs;
+touchDir($config{directory});
 # Login
 my $pandora = WebService::Pandora->new( username => $config{email}, password => $config{password} );
 $pandora->login() or die( $pandora->error() );
