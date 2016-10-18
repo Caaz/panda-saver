7863c526d9ac3dfeb1b2377da851107ed9ce67f2
Daniel Caaz <daniel@caaz.me>
2016-10-18 12:26:36 -0700

    Fix too many arguments.

--- panda-saver.pl
+++ panda-saver.pl
@@ -17,7 +17,7 @@ sub say($$) { print "\e[".shift.'m'.shift."\e[".RESET."m\n" }
 sub sanitize($) { my $text = shift; $text =~ s/[\/]/_/gs; return $text; }
 sub handleError($$) { my ($r, $p) = @_; die $p->error if(!$r); return $r; }
 sub getInput($$) { my $i = shift; print "\e[".YELLOW.'m'.shift.":\e[".RESET."m"; chomp($$i = <STDIN>); }
-sub waitFor($$) { my $waitTime = get_mp3info(shift)->{SECS}-shift; if($waitTime > 0){ say(GRAY,shift); sleep($waitTime); } }
+sub waitFor($$$) { my $waitTime = get_mp3info(shift)->{SECS}-shift; if($waitTime > 0){ say(GRAY,shift); sleep($waitTime); } }
 sub login($) {
   my $p = shift;
   ${$p} = WebService::Pandora->new(username => $config{email}, password => $config{password});
